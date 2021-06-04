13afdc7dd1e22b56456eb2a2265d4cf06cddeebf
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-04 09:56:49 -0300

    refactor: add mongo config

--- src/modules/admin/module.ts
+++ src/modules/admin/module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_DB, MONGO_HOST, MONGO_PORT } from 'app.vars';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 
+const uri = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(uri)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AdminModule {}
