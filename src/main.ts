e9265524cfd6cc3ea0fe9c92a41c578d3aabf462
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-07 10:21:54 -0300

    refactor: add helmet and rateLimit support

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,46 @@
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
-import { CorsOptions } from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { useContainer } from 'typeorm';
 import { AppLogger } from 'app.logger';
+import { enableCors } from 'cors.service';
 import { enableSwagger } from 'swagger.service';
-import { ApplicationModule } from 'modules';
-import { IS_DEV, PORT, HOST } from 'app.vars';
+import { AppModule } from 'modules';
+import { IS_PROD, APP_PORT, APP_HOST } from 'app.constants';
 
 class Main {
   static async bootstrap() {
-    const app = await NestFactory.create(ApplicationModule, {
-      logger: new AppLogger(),
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
+      logger: new AppLogger()
     });
 
-    const corsOptions: CorsOptions = {
-      origin: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      preflightContinue: false,
-      optionsSuccessStatus: 200,
-    };
-    app.enableCors(corsOptions);
     app.useGlobalPipes(
       new ValidationPipe({
-        disableErrorMessages: IS_DEV,
-        forbidUnknownValues: true,
         transform: true,
-      }),
+        disableErrorMessages: IS_PROD,
+        forbidUnknownValues: true,
+        whitelist: true
+      })
     );
+
+    enableCors(app);
 
     enableSwagger(app);
 
+    //is used for allow custom pipes attribute
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+    app.use(helmet());
+    app.use(rateLimit({ windowMs: 60 * 1000, max: 1000 }));
+
     await app
-      .listen(parseInt(PORT.toString(), 10), HOST, () => {
-        Logger.verbose(`Listen on ${PORT} 🙌 `, Main.name);
+      .listen(APP_PORT, APP_HOST, () => {
+        Logger.verbose(`Listen on ${APP_PORT} 🙌 `, Main.name);
       })
-      .catch((error) => Logger.error(error));
+      .catch(error => Logger.error(error));
   }
 }
 
