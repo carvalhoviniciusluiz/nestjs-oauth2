c538399867ea29dfd7c0078f861cda5041e0a4f6
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-08 13:52:04 -0300

    refactor: disable unnecessary modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { RouterModule } from 'nest-router';
 
-import { AdminModule } from './admin/admin.module';
-import { AppModule as ApplicationModule } from './app/app.module';
+// import { AdminModule } from './admin/admin.module';
+// import { AppModule as ApplicationModule } from './app/app.module';
 import { OAuth2Module } from './oauth2/oauth2.module';
+// import { ApplicationModule as OAuth2Module } from './oauth2/application/application.module';
 
 @Module({
   imports: [
     RouterModule.forRoutes([
-      { path: '/admin', module: AdminModule },
-      { path: '/', module: ApplicationModule },
+      // { path: '/admin', module: AdminModule },
+      // { path: '/', module: ApplicationModule },
       { path: '/v1', module: OAuth2Module }
     ]),
-    AdminModule,
-    ApplicationModule,
+    // AdminModule,
+    // ApplicationModule,
     OAuth2Module
   ]
 })
