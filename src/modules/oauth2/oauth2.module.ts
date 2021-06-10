d127efa934b2755b25e6a861bf219d5f386adbb5
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-10 08:14:01 -0300

    refactor: add new structure

--- src/modules/oauth2/oauth2.module.ts
+++ src/modules/oauth2/oauth2.module.ts
@@ -1,13 +1,25 @@
-import { Module, Type } from '@nestjs/common';
+import { Module, OnModuleInit, Type } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER } from 'app.constants';
 import { Oauth2Controller } from './application/controllers';
-import { AccessTokenEntity, ClientEntity, AccessTokenService, ClientService } from './domain';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ClientCredentialsStrategy } from './infrastructure/strategies';
+import { CreateAccessTokenHandler } from './infrastructure/commands';
+import { AccessTokenCreatedEventHandler } from './infrastructure/events';
+import { ClientEntity, AccessTokenEntity } from './infrastructure/entities';
+import { AccessTokenService, ClientService } from './infrastructure/services';
+import { Oauth2GrantStrategyRegistry, StrategyExplorer } from './infrastructure/core';
+import { POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER } from 'app.constants';
 
 const controllers: Type<any>[] = [Oauth2Controller];
 
+export const Oauth2Strategies = [ClientCredentialsStrategy];
+
+export const CommandHandlers = [CreateAccessTokenHandler];
+export const EventHandlers = [AccessTokenCreatedEventHandler];
+
 @Module({
   imports: [
+    CqrsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: POSTGRES_HOST,
@@ -18,7 +30,7 @@ const controllers: Type<any>[] = [Oauth2Controller];
       entities: [`${__dirname}/../**/*.entity.{js,ts}`],
       migrations: [`${__dirname}/../migrations/*.{js,ts}`],
       synchronize: true,
-      logging: true,
+      logging: false,
       cli: {
         migrationsDir: `${__dirname}/../migrations`
       }
@@ -27,9 +39,24 @@ const controllers: Type<any>[] = [Oauth2Controller];
   ],
   providers: [
     { provide: 'ClientServiceInterface', useClass: ClientService },
-    { provide: 'AccessTokenServiceInterface', useClass: AccessTokenService }
+    { provide: 'AccessTokenServiceInterface', useClass: AccessTokenService },
+    StrategyExplorer,
+    Oauth2GrantStrategyRegistry,
+    ...Oauth2Strategies,
+    ...CommandHandlers,
+    ...EventHandlers
   ],
   controllers: [...controllers],
   exports: [TypeOrmModule]
 })
-export class OAuth2Module {}
+export class OAuth2Module implements OnModuleInit {
+  constructor(
+    private readonly explorerService: StrategyExplorer,
+    private readonly strategyRegistry: Oauth2GrantStrategyRegistry
+  ) {}
+
+  onModuleInit() {
+    const { strategies } = this.explorerService.explore();
+    this.strategyRegistry.register(strategies);
+  }
+}
