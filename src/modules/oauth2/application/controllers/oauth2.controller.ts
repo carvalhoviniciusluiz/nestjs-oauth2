ce8f8cafc54fa5f64d4905dfeab8b782b4cde2c1
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-10 08:15:20 -0300

    refactor: add response to request

--- src/modules/oauth2/application/controllers/oauth2.controller.ts
+++ src/modules/oauth2/application/controllers/oauth2.controller.ts
@@ -1,6 +1,15 @@
-import { Body, ClassSerializerInterceptor, Controller, Inject, Post, UseInterceptors } from '@nestjs/common';
-import { ClientServiceInterface } from '../../domain';
-import { OAuth2Request } from '../dtos';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  ForbiddenException,
+  Inject,
+  Post,
+  UseInterceptors
+} from '@nestjs/common';
+import { Oauth2GrantStrategyRegistry } from 'modules/oauth2/infrastructure';
+import { ClientServiceInterface } from 'modules/oauth2/domain';
+import { OAuth2Request } from 'modules/oauth2/application';
 
 @Controller('oauth2')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -9,17 +18,22 @@ export class Oauth2Controller {
    * Constructor
    *
    * @param clientRepository
+   * @param strategyRegistry
    */
   constructor(
     @Inject('ClientServiceInterface')
-    private readonly clientService: ClientServiceInterface
+    private readonly clientService: ClientServiceInterface,
+    private readonly strategyRegistry: Oauth2GrantStrategyRegistry
   ) {}
 
   @Post('token')
   async token(@Body() request: OAuth2Request) {
-    console.log(request);
-
     const client = await this.clientService.findByClientId(request.clientId);
-    console.log(client);
+
+    if (!(await this.strategyRegistry.validate(request, client))) {
+      throw new ForbiddenException('You are not allowed to access the given resource');
+    }
+
+    return await this.strategyRegistry.getOauth2Response(request, client);
   }
 }
