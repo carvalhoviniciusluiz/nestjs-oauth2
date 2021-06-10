5ab4b1da04efffa789d4438f811c11c2343c52b3
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-10 08:08:21 -0300

    refactor: move services folder from domain to infrastructure

---
+++ src/modules/oauth2/infrastructure/services/client.service.ts
@@ -0,0 +1,62 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ClientNotFoundException } from 'modules/oauth2/domain/exceptions';
+import { ClientServiceInterface } from 'modules/oauth2/domain/services';
+import { ClientEntity } from 'modules/oauth2/infrastructure/entities';
+
+@Injectable()
+export class ClientService implements ClientServiceInterface {
+  constructor(
+    @InjectRepository(ClientEntity)
+    private readonly repository: Repository<ClientEntity>
+  ) {}
+
+  async find(id: string): Promise<ClientEntity> {
+    const client = await this.repository.findOne(id);
+
+    if (!client) {
+      throw ClientNotFoundException.withId(id);
+    }
+
+    return client;
+  }
+
+  async findByClientId(clientId: string): Promise<ClientEntity> {
+    const client = await this.repository.findOne({
+      where: {
+        clientId: clientId
+      }
+    });
+
+    if (!client) {
+      throw ClientNotFoundException.withClientId(clientId);
+    }
+
+    return client;
+  }
+
+  async findByName(name: string): Promise<ClientEntity> {
+    const client = await this.repository.findOne({
+      where: {
+        name: name
+      }
+    });
+
+    if (!client) {
+      throw ClientNotFoundException.withName(name);
+    }
+
+    return client;
+  }
+
+  async create(client: ClientEntity): Promise<ClientEntity> {
+    return await this.repository.save(client);
+  }
+
+  async delete(client: ClientEntity): Promise<ClientEntity> {
+    client.deletedAt = new Date();
+
+    return await this.repository.save(client);
+  }
+}
