8fd539eefaf3a7322d8dec555b9edd38e92c55c6
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-06-10 09:24:07 -0300

    feat: add domain to user validator

---
+++ src/modules/oauth2/domain/validators/user-validator.interface.ts
@@ -0,0 +1,17 @@
+import { UserInterface } from '../payloads/user.interface';
+
+/**
+ * Validates that the usernanme exists and has the given password
+ */
+export interface UserValidatorInterface {
+  /**
+   * Implement this method to validate the user existence
+   *
+   * @param username
+   * @param password
+   *
+   * @return UserInterface
+   * @throws InvalidUserException
+   */
+  validate(username: string, password: string): Promise<UserInterface>;
+}
